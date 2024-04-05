7bde0c2654ae8b7a1e9d8be2b114514cf6223e60
ruben.dopico.dev <ruben.dopico.dev@gmail.com>
2024-04-05 17:22:37 +0200

    FIX: corrections on use a main function to use the other functions

--- index.ts
+++ index.ts
@@ -1,12 +1,11 @@
 import fs from 'fs';
 import { Room } from './room';
 
-const readAllRoomsData = (): string => {
-    return fs.readFileSync('./rooms.json').toString();
+const readAllRoomsData = (): Room[] => {
+    return JSON.parse(fs.readFileSync('./rooms.json').toString());
 }
 
-const orderByPriceLowToHigh = (): Room[] => {
-    const allRooms: Room[] = JSON.parse(readAllRoomsData());
+const orderByPriceLowToHigh = (allRooms: Room[]): Room[] => {
     const roomsOrdered = allRooms.sort((room1, room2) => {
         if(room1.price > room2.price) {
             return 1;
@@ -18,15 +17,23 @@ const orderByPriceLowToHigh = (): Room[] => {
     return roomsOrdered;
 }
 
-const roomsToCsv = (): void => {
-    const orderedRooms: Room[] = orderByPriceLowToHigh();
-    let textToWrite: string = Object.keys(orderedRooms[0]).map(header => header.toUpperCase()).join(';');
-    textToWrite += '\n';
-    // id,foto,type,number,description,offer,price,cancellation,amenities,discount,status
+const roomsToCsv = (orderedRooms: Room[]): void => {
+    const roomsKeys = Object.keys(orderedRooms[0]);
+    let textToWrite: string = roomsKeys.map(header => header.toUpperCase()).join(';');
     orderedRooms.forEach(room => {
-        textToWrite += `"${room.id}"; "${room.foto}"; "${room.type}"; "${room.number}"; "${room.description}"; "${room.offer}"; "${room.price}"; "${room.cancellation}"; "${room.amenities.join(',').toString()}"; "${room.discount}"; "${room.status}";\n`; 
+        textToWrite += '\n';
+        roomsKeys.forEach((key) => {
+            const roomKey = key as keyof Room;
+            textToWrite += `"${room[roomKey]}";`;
+        })
     })
     fs.writeFileSync('./rooms.csv', textToWrite);
 }
 
-roomsToCsv();
+const main = () => {
+    const rooms = readAllRoomsData();
+    const roomsOrdered = orderByPriceLowToHigh(rooms);
+    roomsToCsv(roomsOrdered);
+}
+
+main();
